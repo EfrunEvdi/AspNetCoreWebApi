dae05052ef4fb7e4da46189d6617a4318d1b100f
Efrun Evdi <efruncetkin@gmail.com>
2024-02-01 16:12:03 +0300

    4.9. Ders Sonu

---
+++ Applications/basics/BookDemo/Controllers/BooksController.cs
@@ -0,0 +1,114 @@
+﻿using BookDemo.Data;
+using BookDemo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            var books = ApplicationContext.Books;
+            return Ok(books);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetOneBooks([FromRoute(Name = "id")] int id)
+        {
+            var book = ApplicationContext.Books.Where(b => b.Id.Equals(id)).SingleOrDefault();
+
+            if (book is null)
+            {
+                return NotFound(); // 404
+            }
+
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public IActionResult CreateOneBook([FromBody] Book book)
+        {
+            try
+            {
+                if (book is null)
+                {
+                    return BadRequest(); // 400
+                }
+
+                ApplicationContext.Books.Add(book);
+                return StatusCode(201, book);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult UpdateOneBook([FromRoute] int id, [FromBody] Book book)
+        {
+            // Check book?
+            var entity = ApplicationContext.Books.Find(b => b.Id.Equals(id));
+
+            if (entity is null)
+            {
+                return NotFound(); // 404
+            }
+
+            // Check Id
+            if (id != book.Id)
+            {
+                return BadRequest(); // 400
+            }
+
+            ApplicationContext.Books.Remove(entity);
+            book.Id = entity.Id;
+            ApplicationContext.Books.Add(book);
+            return Ok(book);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteAllBook()
+        {
+            ApplicationContext.Books.Clear();
+            return NoContent(); //204
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteOneBook([FromRoute(Name = "id")] int id)
+        {
+            var entity = ApplicationContext.Books.Find(b => b.Id.Equals(id));
+
+            if (entity is null)
+            {
+                return NotFound(new
+                {
+                    statusCode = 404,
+                    message = $"Book with id:{id} could not found."
+                }); // 404
+            }
+
+            ApplicationContext.Books.Remove(entity);
+            return NoContent();
+        }
+
+        [HttpPatch]
+        public IActionResult PartiallyUpdateOneBook([FromRoute(Name = "id")] int id, [FromBody] JsonPatchDocument<Book> bookPatch)
+        {
+            var entity = ApplicationContext.Books.Find(b => b.Id.Equals(id));
+
+            if (entity is null)
+            {
+                return NotFound(); // 404
+            }
+
+            bookPatch.ApplyTo(entity);
+            return NoContent(); // 204
+        }
+    }
+}
