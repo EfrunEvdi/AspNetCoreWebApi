303042618155cd8089e85c233790c86e1c876635
Efrun Evdi <efruncetkin@gmail.com>
2024-02-02 21:26:46 +0300

    5.7. Ders Sonu

--- Applications/basics/BookDemo/Program.cs
+++ Applications/basics/BookDemo/Program.cs
@@ -2,8 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-/*.AddNewtonsoftJson()*/
+builder.Services.AddControllers()
+                .AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
