4e1c6dbce25189869acf1bcaf30c93bc0242db94
Efrun Evdi <efruncetkin@gmail.com>
2024-02-05 21:10:55 +0300

    6.6. Ders Sonu

---
+++ Applications/bsStoreApp/WebApi/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApi.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+    }
+}
---
+++ Applications/bsStoreApp/WebApi/Repositories/RepositoryContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+using WebApi.Repositories.Config;
+
+namespace WebApi.Repositories
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new BookConfig());
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
